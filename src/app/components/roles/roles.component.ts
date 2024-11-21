5ef5c213b73987e04a71769a84af925bc7224698
sagar07 <sagarb.hyd@cnkonline.in>
2024-11-21 10:51:32 +0530

    created components

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,75 @@
+import { HttpClient } from '@angular/common/http';
+import { Component,inject,OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/class/interface/role';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent implements OnInit {
+
+  // roleList:any [] =[];
+  roleList:IRole [] =[];
+  http=inject(HttpClient)
+
+  ngOnInit(): void {
+    this.getAllRoles()
+  }
+
+
+  getAllRoles(){
+    this.http.get("http://127.0.0.1:8000/User_router/get_user").subscribe((res:any)=>{
+      this.roleList=res.result;
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // /////data types
+  // //string,number,boolean,date,object,array,null,undefined
+  // firstNmae:string ="Angular Tutorial";
+  // angularVersion="version 18";
+
+  // version:number = 10;
+  // isActive:boolean = false;
+
+  // currentDate : Date=new Date();// current date
+
+  // inputType:string ="button"; // you can which tag do you want here check,radio,button 
+  // selectedState:string='';
+
+  // //create function in typescript 
+  // showWelcomeAlert(){
+  //   alert("Welcome to Angular Tutorial");
+  // }
+
+  // showMessage(message:string){
+  //   alert(message)
+  // }
+}
